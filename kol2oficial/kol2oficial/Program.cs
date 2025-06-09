4b19a8dd4acbb451b2444a078b5cfd6242dc13fa
s31426 <s31426@pjwstk.edu.pl>
2025-06-09 21:00:46 +0200

    init2

---
+++ kol2oficial/kol2oficial/Program.cs
@@ -0,0 +1,22 @@
+using kol2oficial.Data;
+using kol2oficial.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataBaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<IDbService, DbService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
