975a335cb1fed58e4d85e8d80bc0c772aaa16529
s31426 <s31426@pjwstk.edu.pl>
2025-06-10 17:24:14 +0200

    lekka poprawka

--- kol2oficial/kol2oficial/Services/DbService.cs
+++ kol2oficial/kol2oficial/Services/DbService.cs
@@ -15,33 +15,33 @@ public class DbService : IDbService
 
     public async Task<FinalDTO> GetMatchesById(int playerId)
     {
-        var final = await _context.Player_Matchs.Select(
-            e => new FinalDTO
-            {
-                Player = new PlayerDTO()
-                {
-                    firstName = e.Player.FirstName,
-                    lastName = e.Player.LastName,
-                    birthDate = e.Player.BirthDate,
-                },
-                PlayerMatches = e.Match.Player_Matchs.Select(e => new PlayerMatchesDTO()
-                {
-                    Tournament = new TournamentDTO()
-                    {
-                        tournamentName = e.Match.TournamentId,
-                    },
-                    Map = new MapDTO()
-                    {
-                        mapName = e.Match.Map.Name,
-                    },
-                    Match = new matchDTO()
-                    {
-                        MatchDate = e.Match.MatchDate,
-                        team1Score = e.Match.Team1Score,
-                        team2Score = e.Match.Team2Score,
-                    }
-                }).ToList(),
-            }).FirstOrDefault(e => e.matchId == matchId);
-        return final;
+        //var final = await _context.Player_Matchs.Select(
+           // e => new FinalDTO
+            //{
+              //  Player = new PlayerDTO()
+                //{
+                  //  firstName = e.Player.FirstName,
+                    //lastName = e.Player.LastName,
+                   // birthDate = e.Player.BirthDate,
+                   //  },
+                   //   PlayerMatches = e.Match.Player_Matchs.Select(e => new PlayerMatchesDTO()
+                   //   {
+                   //     Tournament = new TournamentDTO()
+                    //       {
+                    //          tournamentName = e.Match.TournamentId,
+                    //      },
+                    //    Map = new MapDTO()
+                    //    {
+                    //          mapName = e.Match.Map.Name,
+                    //   },
+                    //   Match = new matchDTO()
+                    //   {
+                        //       MatchDate = e.Match.MatchDate,
+                        //       team1Score = e.Match.Team1Score,
+                        //         team2Score = e.Match.Team2Score,
+                        //      }
+                        //   }).ToList()
+       //     }).FirstOrDefault(e => e.matchId == matchId);
+        return null;
     }
 }
