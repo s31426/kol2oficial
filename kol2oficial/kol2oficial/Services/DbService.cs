8c6a1d72033a3ad879a5697ae3c42da362df8f26
Tytus Hrymniak <tytus.hrymniak@vpplant.pl>
2025-06-10 12:32:23 +0200

    dodane linki do pomocy

--- kol2oficial/kol2oficial/Services/DbService.cs
+++ kol2oficial/kol2oficial/Services/DbService.cs
@@ -4,3 +4,6 @@ public class DbService : IDbService
 {
     
 }
+// https://bss.pja.edu.pl/docs/students/linux/mssql_datagrip/
+// https://github.com/mpazio/apbd-2025-example-test-2
+// https://github.com/mpazio/apbd-2024-example-test-2
