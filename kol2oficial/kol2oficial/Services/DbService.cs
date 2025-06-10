06fda4814e5679cc5d3531e06d315537625aa6d0
Tytus Hrymniak <tytus.hrymniak@vpplant.pl>
2025-06-10 13:16:10 +0200

    c

--- kol2oficial/kol2oficial/Services/DbService.cs
+++ kol2oficial/kol2oficial/Services/DbService.cs
@@ -7,3 +7,6 @@ public class DbService : IDbService
 // https://bss.pja.edu.pl/docs/students/linux/mssql_datagrip/
 // https://github.com/mpazio/apbd-2025-example-test-2
 // https://github.com/mpazio/apbd-2024-example-test-2
+
+// dotnet ef migrations add Init
+// dotnet ef database update
